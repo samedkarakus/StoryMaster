743fe2fe2f8809e6d5f4d597d8aa59b9b63e5953
samedkarakus <samedkarakus.ceng@gmail.com>
2024-07-26 16:11:42 +0300

    Button styles updated & bugs fixed.

--- StoryMaster/Controller/ViewController.swift
+++ StoryMaster/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
@@ -17,45 +17,44 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        choice1Button.setTitle("Mağaraya gir", for: .normal)
-        choice2Button.setTitle("Ormanda kal", for: .normal)
         updateUI()
     }
-    
+
     @IBAction func choiceMade(_ sender: UIButton) {
-        guard let choiceTitle = sender.currentTitle else {
-            print("Error: sender.currentTitle is nil")
-            return
-        }
         
-        storyBrain.nextStory(choiceTitle)
-        
-        UIView.animate(withDuration: 0.3, animations: {
-            self.choice1Button.alpha = 0
-            self.choice2Button.alpha = 0
-            self.storyLabel.alpha = 0
-        }) { _ in
-            self.updateUI()
-            
-            UIView.animate(withDuration: 0.3) {
-                self.choice1Button.alpha = 1
-                self.choice2Button.alpha = 1
-                self.storyLabel.alpha = 1
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        UIView.animate(withDuration: 0.3, animations: { [self] in
+            storyLabel.alpha = 0
+            choice1Button.alpha = 0
+            choice2Button.alpha = 0
+        }, completion: { [self] _ in
+            updateUI()
+            UIView.animate(withDuration: 0.3) { [self] in
+                storyLabel.alpha = 1
+                choice1Button.alpha = 1
+                choice2Button.alpha = 1
             }
+        })
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        setButtonCornerRadius(button: choice1Button, radius: 10)
+         setButtonCornerRadius(button: choice2Button, radius: 10)
+    
+        let isDead = storyBrain.deathSituation()
+        
+        if isDead {
+            choice1Button.isHidden = true
+        } else {
+            choice1Button.isHidden = false
         }
     }
     
-    @objc func updateUI() {
-        storyLabel.text = storyBrain.getStoryText()
-        
-        let mediumFont = UIFont.systemFont(ofSize: 16, weight: .medium)
-        
-        let choice1Title = storyBrain.getChoice1()
-        let choice1AttributedTitle = NSAttributedString(string: choice1Title, attributes: [.font: mediumFont])
-        choice1Button.setAttributedTitle(choice1AttributedTitle, for: .normal)
-        
-        let choice2Title = storyBrain.getChoice2()
-        let choice2AttributedTitle = NSAttributedString(string: choice2Title, attributes: [.font: mediumFont])
-        choice2Button.setAttributedTitle(choice2AttributedTitle, for: .normal)
+    func setButtonCornerRadius(button: UIButton, radius: CGFloat) {
+        button.layer.cornerRadius = radius
+        button.clipsToBounds = true
     }
 }
